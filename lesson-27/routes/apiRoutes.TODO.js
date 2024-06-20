201d72f310b9b8c20c8f7295e78a7e2ce7b4aa32
Seungsi <117188786+Seungsi@users.noreply.github.com>
2024-06-20 12:55:21 +0000

    27끝

--- lesson-27/routes/apiRoutes.TODO.js
+++ lesson-27/routes/apiRoutes.TODO.js
@@ -5,3 +5,21 @@
  * Listing 27.1 (p. 392)
  * @TODO: apiRoutes.js에서 모든 강좌를 보기 위한 라우트 추가
  */
+const router = require("express").Router(),
+  coursesController = require("../controllers/coursesController");
+
+
+router.get(
+  "/courses",
+  coursesController.index,
+  coursesController.filterUserCourses, // @TODO: Listing 27.7 (p. 401)
+  coursesController.respondJSON
+);
+router.get(
+  "/courses/:id/join",
+  coursesController.join,
+  coursesController.respondJSON
+);
+router.use(coursesController.errorJSON);
+
+module.exports = router;
